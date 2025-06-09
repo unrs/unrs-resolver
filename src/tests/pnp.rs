ee31e61f93eb0d89f0a73834246e577adc3676a3
JounQin <admin@1stg.me>
2025-06-09 22:18:34 +0800

    fix: npm protocol alias with pnp should be supported

--- src/tests/pnp.rs
+++ src/tests/pnp.rs
@@ -144,6 +144,20 @@ fn resolve_npm_protocol_alias() {
             ".yarn/cache/minimist-npm-1.2.8-d7af7b1dce-19d3fcdca0.zip/node_modules/minimist/index.js"
         ))
     );
+
+    assert_eq!(
+        resolver.resolve(&fixture, "@custom/pragmatic-drag-and-drop").map(|r| r.full_path()),
+        Ok(fixture.join(
+            ".yarn/cache/@atlaskit-pragmatic-drag-and-drop-npm-1.5.2-3241d4f843-1dace49fa3.zip/node_modules/@atlaskit/pragmatic-drag-and-drop/dist/cjs/index.js"
+        ))
+    );
+
+    assert_eq!(
+        resolver.resolve(&fixture, "pragmatic-drag-and-drop").map(|r| r.full_path()),
+        Ok(fixture.join(
+            ".yarn/cache/@atlaskit-pragmatic-drag-and-drop-npm-1.5.2-3241d4f843-1dace49fa3.zip/node_modules/@atlaskit/pragmatic-drag-and-drop/dist/cjs/index.js"
+        ))
+    );
 }
 
 // Windows is blocked by upstream
