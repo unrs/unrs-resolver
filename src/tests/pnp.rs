9cc7b7a1faf6b124d891dfcbd23ea7a5b9765704
JounQin <admin@1stg.me>
2025-06-09 21:46:36 +0800

    test: add failing test case for npm protocol alias with pnp

--- src/tests/pnp.rs
+++ src/tests/pnp.rs
@@ -132,6 +132,20 @@ fn resolve_pnp_nested_package_json() {
     );
 }
 
+#[test]
+fn resolve_npm_protocol_alias() {
+    let fixture = super::fixture_root().join("pnp");
+
+    let resolver = Resolver::default();
+
+    assert_eq!(
+        resolver.resolve(&fixture, "custom-minimist").map(|r| r.full_path()),
+        Ok(fixture.join(
+            ".yarn/cache/minimist-npm-1.2.8-d7af7b1dce-19d3fcdca0.zip/node_modules/minimist/index.js"
+        ))
+    );
+}
+
 // Windows is blocked by upstream
 // see also https://github.com/yarnpkg/pnp-rs/pull/10
 #[cfg(not(windows))]
